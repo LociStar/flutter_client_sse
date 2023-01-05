6936f64aaa388732e11ad271652c7bcb4f4866d1
LociStar <locistar.got@gmail.com>
2023-01-05 15:59:16 +0100

    updated example

--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter_client_sse/flutter_client_sse.dart';
 
 void main() {
-  SSEClient.subscribeToSSE(
-      url:
-          'http://192.168.1.2:3000/api/activity-stream?historySnapshot=FIVE_MINUTE',
-      header: {
-        "Cookie":
-            'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NDMyMTAyMzEsImV4cCI6MTY0MzgxNTAzMX0.U0aCAM2fKE1OVnGFbgAU_UVBvNwOMMquvPY8QaLD138; Path=/; Expires=Wed, 02 Feb 2022 15:17:11 GMT; HttpOnly; SameSite=Strict',
-        "Accept": "text/event-stream",
-        "Cache-Control": "no-cache",
-      }).listen((event) {
+  SSEClient sseClient = new SSEClient();
+
+  sseClient.subscribeToSSE(url: 'http://192.168.2.161:8085/getDataStream', header: {
+    "Cache-Control": "no-cache",
+    "Connection": "Keep-Alive",
+  }).listen((event) {
     print('Id: ' + event.id!);
     print('Event: ' + event.event!);
     print('Data: ' + event.data!);
